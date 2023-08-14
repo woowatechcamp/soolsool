6404ffa7808ee88b9e68106bb6594f5b8ff6dd08
whatasame <whatasame@gmail.com>
2023-08-15 01:45:38 +0900

    build: 프로젝트 최초 생성

---
+++ src/main/java/com/woowacamp/soolsool/SoolsoolApplication.java
@@ -0,0 +1,13 @@
+package com.woowacamp.soolsool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoolsoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoolsoolApplication.class, args);
+    }
+
+}
