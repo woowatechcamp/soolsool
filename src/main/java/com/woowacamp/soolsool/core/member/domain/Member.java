1ee7e933ee337813836bd116f2034b64c7abe840
[TBO] SeKwun, JUNG <85625910+jjungsk@users.noreply.github.com>
2023-08-15 17:16:20 +0900

    feat: prod, dev, test 환경 변수 파일 추가, MemberEntity 추가, 패키지 분리 (#21)
    Co-authored-by: Jeonggyu Choi <whatasame@gmail.com>

---
+++ src/main/java/com/woowacamp/soolsool/core/member/domain/Member.java
@@ -0,0 +1,58 @@
+package com.woowacamp.soolsool.core.member.domain;
+
+import com.woowacamp.soolsool.global.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private RoleType role;
+
+    @Builder
+    public Member(
+        String email,
+        String password,
+        String phoneNumber,
+        String address,
+        RoleType role
+    ) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+}
