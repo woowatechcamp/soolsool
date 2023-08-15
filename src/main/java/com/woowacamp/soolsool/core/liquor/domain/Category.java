8f9878a79776376dce429b5deb3e2e17a6ec13be
worldii <bbc2788@naver.com>
2023-08-15 23:03:39 +0900

    Feat : Category 엔티티 구현

--- src/main/java/com/woowacamp/soolsool/core/liquor/domain/Category.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/domain/Category.java
@@ -1,4 +1,32 @@
 package com.woowacamp.soolsool.core.liquor.domain;
 
-public enum Category {
+import com.woowacamp.soolsool.global.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    private String type;
+
+    @Builder
+    public Category(String type) {
+        this.type = type;
+    }
 }
