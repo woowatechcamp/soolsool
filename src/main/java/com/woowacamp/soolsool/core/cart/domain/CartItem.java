88b3d712516e283a64f08fe7a4a383908dc61d61
worldii <bbc2788@naver.com>
2023-08-15 22:51:44 +0900

    Feat : CartItem 엔티티 구현

--- src/main/java/com/woowacamp/soolsool/core/cart/domain/CartItem.java
+++ src/main/java/com/woowacamp/soolsool/core/cart/domain/CartItem.java
@@ -1,19 +1,45 @@
 package com.woowacamp.soolsool.core.cart.domain;
 
+import static lombok.AccessLevel.PROTECTED;
+
 import com.woowacamp.soolsool.core.liquor.domain.Liquor;
 import com.woowacamp.soolsool.global.common.BaseEntity;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "cart_item")
-@NoArgsConstructor
+@Table(name = "cart_items")
+@NoArgsConstructor(access = PROTECTED)
 public class CartItem extends BaseEntity {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "member_id")
     private Long memberId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "liquor_id")
+
     private Liquor liquor;
+
+    @Column(name = "liquor_quantity")
     private int quantity;
 
+    @Builder
+    public CartItem(Long memberId, Liquor liquor, int quantity) {
+        this.memberId = memberId;
+        this.liquor = liquor;
+        this.quantity = quantity;
+    }
 }
--- src/main/java/com/woowacamp/soolsool/core/liquor/domain/Region.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/domain/Region.java
@@ -1,4 +1,17 @@
 package com.woowacamp.soolsool.core.liquor.domain;
 
+import javax.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Region {
+
+    GYEONGGIDO("경기도");
+    
+    @Column(name = "name", length = 20)
+    private String name;
+
+
 }
