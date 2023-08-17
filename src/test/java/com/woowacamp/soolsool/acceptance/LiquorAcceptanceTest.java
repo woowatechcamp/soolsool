6a1dbbbec7fd9bd33b90b7082ccd2d6ecf59dfb0
worldii <bbc2788@naver.com>
2023-08-18 02:30:57 +0900

    test: Liquor 수정 인수테스트 추가

--- src/test/java/com/woowacamp/soolsool/acceptance/LiquorAcceptanceTest.java
+++ src/test/java/com/woowacamp/soolsool/acceptance/LiquorAcceptanceTest.java
@@ -3,8 +3,10 @@ package com.woowacamp.soolsool.acceptance;
 import static com.woowacamp.soolsool.global.common.LiquorResultCode.LIQUOR_CREATED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 import com.woowacamp.soolsool.acceptance.helper.RestAssuredHelper.Liquor;
+import com.woowacamp.soolsool.core.liquor.dto.ModifyLiquorRequest;
 import com.woowacamp.soolsool.core.liquor.dto.SaveLiquorRequest;
 import com.woowacamp.soolsool.global.common.ApiResponse;
 import io.restassured.response.ExtractableResponse;
@@ -43,4 +45,43 @@ class LiquorAcceptanceTest extends AcceptanceTest {
         assertThat(actual.getMessage())
             .isEqualTo(LIQUOR_CREATED.getMessage());
     }
+
+    @Test
+    @DisplayName("술 상품을 수정할 수 있다.")
+    void modifyLiquorTest() {
+        // given
+        String accessToken = "1234";
+        SaveLiquorRequest saveLiquorRequest = new SaveLiquorRequest(
+            "SOJU",
+            "GYEONGGI_DO",
+            "ON_SALE",
+            "새로",
+            "3000",
+            "브랜드",
+            "/url",
+            100, 12.0,
+            300);
+        ExtractableResponse<Response> saveLiquor = Liquor.saveLiquor(accessToken,
+            saveLiquorRequest);
+        ModifyLiquorRequest modifyLiquorRequest = new ModifyLiquorRequest(
+            "SOJU",
+            "GYEONGGI_DO",
+            "ON_SALE",
+            "안동소주",
+            "일품",
+            "브랜드",
+            "soolsool.png",
+            100,
+            12.0,
+            1
+        );
+
+        // when
+        ExtractableResponse<Response> response = Liquor.modifyLiquor(
+            accessToken,
+            modifyLiquorRequest
+        );
+        assertThat(response.statusCode()).isEqualTo(OK.value());
+    }
+
 }
--- src/test/java/com/woowacamp/soolsool/acceptance/helper/RestAssuredHelper.java
+++ src/test/java/com/woowacamp/soolsool/acceptance/helper/RestAssuredHelper.java
@@ -1,5 +1,6 @@
 package com.woowacamp.soolsool.acceptance.helper;
 
+import com.woowacamp.soolsool.core.liquor.dto.ModifyLiquorRequest;
 import com.woowacamp.soolsool.core.liquor.dto.SaveLiquorRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -24,5 +25,18 @@ public class RestAssuredHelper {
                 .extract();
         }
 
+        public static ExtractableResponse<Response> modifyLiquor(
+            String accessToken,
+            ModifyLiquorRequest modifyLiquorRequest) {
+            return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
+                .body(modifyLiquorRequest)
+                .when().put("/liquors")
+                .then().log().all()
+                .extract();
+        }
     }
 }
