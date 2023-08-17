e5b63c78e590be723bde7a97a243401da5ff3716
worldii <bbc2788@naver.com>
2023-08-18 01:39:42 +0900

    feat: Liquor를 저장하는 Controller 구현

---
+++ src/main/java/com/woowacamp/soolsool/core/liquor/controller/LiquorController.java
@@ -0,0 +1,30 @@
+package com.woowacamp.soolsool.core.liquor.controller;
+
+import static com.woowacamp.soolsool.global.common.LiquorResultCode.LIQUOR_CREATED;
+
+import com.woowacamp.soolsool.core.liquor.dto.SaveLiquorRequest;
+import com.woowacamp.soolsool.core.liquor.service.LiquorService;
+import com.woowacamp.soolsool.global.common.ApiResponse;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/liquors")
+public class LiquorController {
+
+    private final LiquorService liquorService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Long>> saveLiquor(
+        @RequestBody final SaveLiquorRequest saveLiquorRequest) {
+        Long saveLiquorId = liquorService.saveLiquor(saveLiquorRequest);
+        return ResponseEntity.created(URI.create("/liquors/" + saveLiquorId))
+            .body(ApiResponse.of(LIQUOR_CREATED, null));
+    }
+}
