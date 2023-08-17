99190640df09f34d0e5ce17ece36d7ae0a2da2d2
worldii <bbc2788@naver.com>
2023-08-17 20:32:41 +0900

    test: LiquorAcceptanceTest 인수테스트 추가

---
+++ src/main/java/com/woowacamp/soolsool/core/liquor/dto/SaveLiquorRequest.java
@@ -0,0 +1,21 @@
+package com.woowacamp.soolsool.core.liquor.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class SaveLiquorRequest {
+
+    private final String typeName;
+    private final String regionName;
+    private final String statusName;
+    private final String name;
+    private final String price;
+    private final String brand;
+    private final String imageUrl;
+    private final Integer stock;
+    private final Double alcohol;
+    private final Integer volume;
+    
+}
--- src/test/java/com/woowacamp/soolsool/acceptance/LiquorAcceptanceTest.java
+++ src/test/java/com/woowacamp/soolsool/acceptance/LiquorAcceptanceTest.java
@@ -1,6 +1,39 @@
 package com.woowacamp.soolsool.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.woowacamp.soolsool.acceptance.helper.RestAssuredHelper.Liquor;
+import com.woowacamp.soolsool.core.liquor.dto.SaveLiquorRequest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+@DisplayName("술 상품 관련 기능")
 class LiquorAcceptanceTest extends AcceptanceTest {
 
-    
+
+    @Test
+    @DisplayName("술 상품을 등록할 수 있다")
+    void saveLiquor() {
+        // given
+        SaveLiquorRequest saveLiquorRequest = new SaveLiquorRequest(
+            "소주",
+            "경기도",
+            "판매중",
+            "새로", "3000",
+            "브랜드", "/url",
+            100, 12.0,
+            300);
+
+        // when
+        ExtractableResponse<Response> response = Liquor.saveLiquor(
+            "1234",
+            saveLiquorRequest
+        );
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
 }
