5c77ef51f507feb0b8b6824dc4af7d4b549176b3
jjungk <jsk33a@gmail.com>
2023-08-17 21:18:25 +0900

    feat: GlobalExceptionHandler @Validation handler 추가

--- src/main/java/com/woowacamp/soolsool/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/woowacamp/soolsool/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.woowacamp.soolsool.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,17 @@ public class GlobalExceptionHandler {
         log.error("message : {}", e.getMessage());
 
         final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse errorResponse = ErrorResponse.from(errorCode);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleValidationException(
+        final MethodArgumentNotValidException exception) {
+        log.error("Validation 에러 : {}", exception.getMessage());
+
+        final ErrorCode errorCode = DefaultErrorCode.DEFAULT_VALIDATION_ERROR;
         final ErrorResponse errorResponse = ErrorResponse.from(errorCode);
 
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
