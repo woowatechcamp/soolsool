daf56d8a92749010f08719ad2e6b03b7e799660f
jjungk <jsk33a@gmail.com>
2023-08-18 20:00:48 +0900

    feat: MemberMileageCharge, MemberMileageUsage entity 추가

---
+++ src/main/java/com/woowacamp/soolsool/core/member/domain/MemberMileageUsage.java
@@ -0,0 +1,56 @@
+package com.woowacamp.soolsool.core.member.domain;
+
+import com.woowacamp.soolsool.core.member.domain.converter.MemberMileageConverter;
+import com.woowacamp.soolsool.core.member.domain.vo.MemberMileage;
+import com.woowacamp.soolsool.core.order.domain.Order;
+import com.woowacamp.soolsool.global.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member_mileage_usages")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberMileageUsage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(name = "amount")
+    @Convert(converter = MemberMileageConverter.class)
+    private MemberMileage mileage;
+
+    @Builder
+    public MemberMileageUsage(
+        final Member member,
+        final Order order,
+        final MemberMileage mileage
+    ) {
+        this.member = member;
+        this.order = order;
+        this.mileage = mileage;
+    }
+}
