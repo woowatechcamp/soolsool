a0095c99448046957dd8d0f345fed6528a2066df
worldii <bbc2788@naver.com>
2023-08-19 03:15:25 +0900

    feat: 이메일로, member의 레포지토리 조회한다.

--- src/main/java/com/woowacamp/soolsool/core/member/repository/MemberRepository.java
+++ src/main/java/com/woowacamp/soolsool/core/member/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.woowacamp.soolsool.core.member.repository;
 
 import com.woowacamp.soolsool.core.member.domain.Member;
+import com.woowacamp.soolsool.core.member.domain.vo.MemberEmail;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(final MemberEmail email);
 }
---
+++ src/test/java/com/woowacamp/soolsool/integration/repository/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.woowacamp.soolsool.integration.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.woowacamp.soolsool.core.member.domain.Member;
+import com.woowacamp.soolsool.core.member.domain.vo.MemberEmail;
+import com.woowacamp.soolsool.core.member.repository.MemberRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("이메일로 member를 조회한다.")
+    void memberCreateTest() {
+        // given
+        final Optional<Member> member = memberRepository.findByEmail(
+            new MemberEmail("woowafriends@naver.com"));
+        // when & then
+        assertThat(member).isNotEmpty();
+    }
+
+}
