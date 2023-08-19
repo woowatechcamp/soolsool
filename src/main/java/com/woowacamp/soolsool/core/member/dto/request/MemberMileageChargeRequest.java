90b6ee2ab77e96f148d6fcc44f3ba175e0055c49
jjungk <jsk33a@gmail.com>
2023-08-19 16:46:57 +0900

    feat: MemberMileageChargeRequest Dto 생성

---
+++ src/main/java/com/woowacamp/soolsool/core/member/dto/request/MemberMileageChargeRequest.java
@@ -0,0 +1,13 @@
+package com.woowacamp.soolsool.core.member.dto.request;
+
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MemberMileageChargeRequest {
+
+    @Size(max = 255, message = "불가능한 마일리지 액수 입니다.")
+    private final String amount;
+}
