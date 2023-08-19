030ea85c19a403bd93b443068ad4610d31e43f5f
jjungk <jsk33a@gmail.com>
2023-08-19 18:25:59 +0900

    refactor: 필드 값 1개인 Dto로 기본생성자, 전체 생성자 추가

--- src/main/java/com/woowacamp/soolsool/core/member/dto/request/MemberMileageChargeRequest.java
+++ src/main/java/com/woowacamp/soolsool/core/member/dto/request/MemberMileageChargeRequest.java
@@ -3,15 +3,17 @@ package com.woowacamp.soolsool.core.member.dto.request;
 import com.woowacamp.soolsool.core.member.domain.Member;
 import com.woowacamp.soolsool.core.member.domain.MemberMileageCharge;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberMileageChargeRequest {
 
     @Size(max = 255, message = "불가능한 마일리지 액수 입니다.")
-    private final String amount;
+    private String amount;
 
     public MemberMileageCharge toMemberMileageCharge(final Member member) {
         return MemberMileageCharge.builder()
