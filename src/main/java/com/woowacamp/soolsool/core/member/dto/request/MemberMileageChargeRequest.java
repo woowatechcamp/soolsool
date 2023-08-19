daf628e2f13d10318fa31b62388963ec203e2de1
jjungk <jsk33a@gmail.com>
2023-08-19 17:35:19 +0900

    feat: MemberService 마일리지 충전 로직 추가

--- src/main/java/com/woowacamp/soolsool/core/member/dto/request/MemberMileageChargeRequest.java
+++ src/main/java/com/woowacamp/soolsool/core/member/dto/request/MemberMileageChargeRequest.java
@@ -1,5 +1,7 @@
 package com.woowacamp.soolsool.core.member.dto.request;
 
+import com.woowacamp.soolsool.core.member.domain.Member;
+import com.woowacamp.soolsool.core.member.domain.MemberMileageCharge;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,4 +12,11 @@ public class MemberMileageChargeRequest {
 
     @Size(max = 255, message = "불가능한 마일리지 액수 입니다.")
     private final String amount;
+
+    public MemberMileageCharge toMemberMileageCharge(final Member member) {
+        return MemberMileageCharge.builder()
+            .member(member)
+            .amount(this.amount)
+            .build();
+    }
 }
