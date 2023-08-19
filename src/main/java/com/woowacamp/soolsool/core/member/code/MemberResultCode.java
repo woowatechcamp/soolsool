541fda5fccbf03ff16938e4dfc2fbaeceb31d66a
jjungk <jsk33a@gmail.com>
2023-08-19 14:08:23 +0900

    feat: MemberResultCode 추가

---
+++ src/main/java/com/woowacamp/soolsool/core/member/code/MemberResultCode.java
@@ -0,0 +1,20 @@
+package com.woowacamp.soolsool.core.member.code;
+
+import com.woowacamp.soolsool.global.common.ResultCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberResultCode implements ResultCode {
+
+    MEMBER_CREATE_SUCCESS(200, "MS201", "회원 등록이 완료되었습니다."),
+    MEMBER_FIND_SUCCESS(200, "MS202", "회원 조회가 완료되었습니다."),
+    MEMBER_MODIFY_SUCCESS(200, "MS203", "회원 정보 수정 완료되었습니다."),
+    MEMBER_DELETE_SUCCESS(204, "MS204", "회원 정보 삭제가 완료되었습니다."),
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
