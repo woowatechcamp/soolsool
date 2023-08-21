48c523e7d96dbadeff2fc505fcc61c845d96ab9e
worldii <bbc2788@naver.com>
2023-08-21 20:42:15 +0900

    refactor: Cart 장바구니 상품 총합 생성 로직 구현

--- src/main/java/com/woowacamp/soolsool/core/cart/domain/Cart.java
+++ src/main/java/com/woowacamp/soolsool/core/cart/domain/Cart.java
@@ -7,6 +7,7 @@ import static com.woowacamp.soolsool.core.cart.code.CartErrorCode.NULL_LIQUOR;
 import static com.woowacamp.soolsool.core.cart.code.CartErrorCode.STOPPED_LIQUOR;
 
 import com.woowacamp.soolsool.global.exception.SoolSoolException;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -72,4 +73,15 @@ public class Cart {
         return cartItems.stream()
             .collect(Collectors.toUnmodifiableList());
     }
+
+    public BigInteger getTotalAmount() {
+        return cartItems.stream()
+            .map(CartItem::getTotalAmount)
+            .reduce(BigInteger.ZERO, BigInteger::add);
+    }
+
+    public int cartItemSize() {
+        return cartItems.size();
+    }
+
 }
