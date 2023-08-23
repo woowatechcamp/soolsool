58bc624cd945aeaf20ee42efb4842c121028bc62
whatasame <97666463+whatasame@users.noreply.github.com>
2023-08-24 01:25:11 +0900

    refactor: Auth 테스트 수정

--- src/test/java/com/woowacamp/soolsool/global/auth/service/AuthServiceIntegrationTest.java
+++ src/test/java/com/woowacamp/soolsool/global/auth/service/AuthServiceIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @Import({AuthService.class, TokenProvider.class})
@@ -22,10 +23,11 @@ class AuthServiceIntegrationTest {
     private AuthService authService;
 
     @Test
+    @Sql({"/member-type.sql", "/member.sql"})
     @DisplayName("이메일과 비밀번호가 일치하면 token을 발급한다.")
     void createTokenWithEmailAndPassword() {
         // given
-        LoginRequest loginRequest = new LoginRequest("woowafriends@naver.com", "woowa");
+        LoginRequest loginRequest = new LoginRequest("kim@email.com", "baedal");
 
         // when & then
         assertThatCode(() -> authService.createToken(loginRequest))
@@ -33,12 +35,13 @@ class AuthServiceIntegrationTest {
     }
 
     @Test
+    @Sql({"/member-type.sql", "/member.sql"})
     @DisplayName("이메일이 일치하지 않으면 토큰 발급에 실패한다")
     void throwErrorWithWrongEmail() {
         // given
         LoginRequest loginRequest = new LoginRequest(
-            "wrongwoowafriends@naver.com",
-            "woowa"
+            "wrong@email.com",
+            "baedal"
         );
 
         // when & then
@@ -48,12 +51,13 @@ class AuthServiceIntegrationTest {
     }
 
     @Test
+    @Sql({"/member-type.sql", "/member.sql"})
     @DisplayName("비밀번호가 일치하지 않으면 토큰 발급에 실패한다")
     void throwErrorWithWrongPassword() {
         // given
         LoginRequest loginRequest = new LoginRequest(
-            "woowafriends@naver.com",
-            "wrongwoowa"
+            "kim@email.com",
+            "wrong"
         );
 
         // when & then
