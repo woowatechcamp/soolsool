c08163bdf8c5cdb92e50ebca499482b3f5074330
whatasame <97666463+whatasame@users.noreply.github.com>
2023-08-24 00:11:18 +0900

    refactor: 회원 시간 데이터 추가

--- src/test/resources/member.sql
+++ src/test/resources/member.sql
@@ -1,17 +1,5 @@
-insert into members (id,
-                     role_id,
-                     email,
-                     password,
-                     name,
-                     phone_number,
-                     mileage,
-                     address)
+insert into members (id, role_id, email, password, name, phone_number, mileage, address,
+                     created_at, modified_at)
 
-values (1,
-        1,
-        'kim@email.com',
-        'baedal',
-        '김배달',
-        '010-0000-0000',
-        '0',
-        '잠실역');
+values (1, 1, 'kim@email.com', 'baedal', '김배달', '010-0000-0000', '0', '잠실역',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
