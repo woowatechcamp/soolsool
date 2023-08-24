637bbad544a890881645244999a316079f430186
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-08-24 10:25:15 +0900

    Update src/main/java/com/woowacamp/soolsool/global/auth/code/AuthErrorCode.java
    Co-authored-by: hwihwi523 <57697202+hwihwi523@users.noreply.github.com>

--- src/main/java/com/woowacamp/soolsool/global/auth/code/AuthErrorCode.java
+++ src/main/java/com/woowacamp/soolsool/global/auth/code/AuthErrorCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 public enum AuthErrorCode implements ErrorCode {
 
     TOKEN_ERROR(BAD_REQUEST.value(), "A101", "토큰이 유효하지 않습니다."),
-    INVALID_AUTHORITY(UNAUTHORIZED.value(), "A102", "권한이 없습니다.");
+    INVALID_AUTHORITY(UNAUTHORIZED.value(), "A102", "권한이 없습니다."),
+    ;
 
     private final int status;
     private final String code;
