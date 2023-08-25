675705bb6363fca693c8b05409b31111057ac24a
whatasame <97666463+whatasame@users.noreply.github.com>
2023-08-25 10:06:47 +0900

    refactor: 메시지 가독성 향상

--- src/main/java/com/woowacamp/soolsool/core/liquor/code/LiquorStockErrorCode.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/code/LiquorStockErrorCode.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 public enum LiquorStockErrorCode implements ErrorCode {
 
     INCLUDE_OTHER_LIQUOR(BAD_REQUEST.value(), "LS101", "동일한 주류가 아닌 재고가 포함되어 있습니다."),
-    EMPTY_LIQUOR_STOCKS(BAD_REQUEST.value(), "LS102", "주류 재고가 비어있습니다."),
+    EMPTY_LIQUOR_STOCKS(BAD_REQUEST.value(), "LS102", "주류 재고가 존재하지 않습니다."),
     NOT_ENOUGH_LIQUOR_STOCKS(BAD_REQUEST.value(), "LS103", "주류 재고가 부족합니다."),
     ;
 
