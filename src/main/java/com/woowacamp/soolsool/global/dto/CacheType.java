ca0465830ff1d8fbf936dd94fadbe90e4d84551f
worldii <bbc2788@naver.com>
2023-08-25 20:16:10 +0900

    feat: 캐쉬 이넘 타입 dto

---
+++ src/main/java/com/woowacamp/soolsool/global/dto/CacheType.java
@@ -0,0 +1,24 @@
+package com.woowacamp.soolsool.global.dto;
+
+import lombok.Getter;
+
+@Getter
+public enum CacheType {
+    LIQUOR_STATUS("liquorStatus", 10, 100),
+    LIQUOR_BREW("liquorBrew", 10, 100),
+    LIQUOR_REGION("liquorRegion", 10, 100),
+    LIQUOR_TYPE("liquorType", 10, 100),
+    MEMBER_ROLE("memberRole", 10, 100),
+    ORDER_STATUS("orderStatus", 10, 100),
+    RECEIPT_STATUS("receiptStatus", 10, 100);
+
+    CacheType(final String cacheName, final int expireAfterWrite, final int maximumSize) {
+        this.cacheName = cacheName;
+        this.expireAfterWrite = expireAfterWrite;
+        this.maximumSize = maximumSize;
+    }
+
+    private final String cacheName;
+    private final int expireAfterWrite;
+    private final int maximumSize;
+}
