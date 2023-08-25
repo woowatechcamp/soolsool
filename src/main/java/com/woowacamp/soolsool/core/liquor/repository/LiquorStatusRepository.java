c6b2bd235ab4e19fe49aa441f5283f357ccb66be
worldii <bbc2788@naver.com>
2023-08-25 20:15:56 +0900

    feat: repository 카테고리 캐쉬 적용

--- src/main/java/com/woowacamp/soolsool/core/liquor/repository/LiquorStatusRepository.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/repository/LiquorStatusRepository.java
@@ -3,11 +3,13 @@ package com.woowacamp.soolsool.core.liquor.repository;
 import com.woowacamp.soolsool.core.liquor.domain.LiquorStatus;
 import com.woowacamp.soolsool.core.liquor.domain.vo.LiquorStatusType;
 import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LiquorStatusRepository extends JpaRepository<LiquorStatus, Long> {
 
+    @Cacheable(value = "liquorStatus", key = "#type")
     Optional<LiquorStatus> findByType(final LiquorStatusType type);
 }
--- src/main/java/com/woowacamp/soolsool/core/order/repository/OrderStatusRepository.java
+++ src/main/java/com/woowacamp/soolsool/core/order/repository/OrderStatusRepository.java
@@ -3,11 +3,13 @@ package com.woowacamp.soolsool.core.order.repository;
 import com.woowacamp.soolsool.core.order.domain.OrderStatus;
 import com.woowacamp.soolsool.core.order.domain.vo.OrderStatusType;
 import java.util.Optional;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
 
+    @Cacheable(value = "orderStatus", key = "#type")
     Optional<OrderStatus> findByType(final OrderStatusType type);
 }
