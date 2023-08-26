dad9264eeab455daa8788c538edab88788e0b123
whatasame <97666463+whatasame@users.noreply.github.com>
2023-08-27 03:33:12 +0900

    test: 인수 테스트 추가

---
+++ src/main/java/com/woowacamp/soolsool/core/liquor/dto/response/LiquorCtrDetailResponse.java
@@ -0,0 +1,11 @@
+package com.woowacamp.soolsool.core.liquor.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class LiquorCtrDetailResponse {
+
+    private final double rate;
+}
