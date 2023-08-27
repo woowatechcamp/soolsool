da28dba3e05ea5a8392b5051d798c75c8be54973
worldii <bbc2788@naver.com>
2023-08-27 17:41:25 +0900

    refactor : fe 연동 위해 수정

--- src/main/java/com/woowacamp/soolsool/core/payment/infra/dto/response/KakaoPayApproveResponse.java
+++ src/main/java/com/woowacamp/soolsool/core/payment/infra/dto/response/KakaoPayApproveResponse.java
@@ -1,5 +1,6 @@
 package com.woowacamp.soolsool.core.payment.infra.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.woowacamp.soolsool.core.payment.dto.response.PayApproveResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KakaoPayApproveResponse {
 
+    @JsonProperty("payment_method_type")
     private final String paymentMethodType;
+    @JsonProperty("card_info")
     private final KakaoPayCardInfo cardInfo;
 
     public PayApproveResponse toPayApproveResponse() {
