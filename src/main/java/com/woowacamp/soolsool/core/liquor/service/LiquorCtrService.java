aefe66d9ba63c0c5c5c73aa9a6ebd5ac9763316b
whatasame <97666463+whatasame@users.noreply.github.com>
2023-08-28 11:02:48 +0900

    refactor: Transactional을 놓치기 싫어

--- src/main/java/com/woowacamp/soolsool/core/liquor/service/LiquorCtrService.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/service/LiquorCtrService.java
@@ -5,6 +5,7 @@ import com.woowacamp.soolsool.core.liquor.repository.LiquorCtrRepository;
 import com.woowacamp.soolsool.global.exception.SoolSoolException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class LiquorCtrService {
 
     private final LiquorCtrRepository liquorCtrRepository;
 
+    @Transactional(readOnly = true)
     public double getLiquorCtrByLiquorId(final Long liquorId) {
         return liquorCtrRepository.findByLiquorId(liquorId)
             .orElseThrow(() -> new SoolSoolException(LiquorCtrErrorCode.NOT_LIQUOR_CTR_FOUND))
