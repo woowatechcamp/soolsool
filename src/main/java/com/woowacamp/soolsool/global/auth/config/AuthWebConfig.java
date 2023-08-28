e4eadc58b6b986bdc978d4638c62de036d6bb9a4
Jeonggyu Choi <97666463+whatasame@users.noreply.github.com>
2023-08-28 14:07:59 +0900

    build: Spring actuator, prometheus 추가 (#161)

--- src/main/java/com/woowacamp/soolsool/global/auth/config/AuthWebConfig.java
+++ src/main/java/com/woowacamp/soolsool/global/auth/config/AuthWebConfig.java
@@ -20,7 +20,8 @@ public class AuthWebConfig implements WebMvcConfigurer {
             .addPathPatterns("/**")
             .excludePathPatterns("/auth/login", "/members")
             .excludePathPatterns("/pay/success/**")
-            .excludePathPatterns("/error", "/css/**", "/assets/**", "/js/**", "/*.ico");
+            .excludePathPatterns("/error", "/css/**", "/assets/**", "/js/**", "/*.ico")
+            .excludePathPatterns("/metrics", "/actuator/**");
     }
 
     @Override
