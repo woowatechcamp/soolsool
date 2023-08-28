cbfff31c0325e2b0c16a9649e6d811d47a512e7f
hwihwi523 <57697202+hwihwi523@users.noreply.github.com>
2023-08-28 15:22:37 +0900

    feat: Redisson 설치 (#159)
    * feat: Redisson 설치
    
    * feat: Redis Host 환경변수 주입
    
    * refactor: 환경변수 수정

---
+++ src/main/java/com/woowacamp/soolsool/global/config/RedissonConfig.java
@@ -0,0 +1,33 @@
+package com.woowacamp.soolsool.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    private final String host;
+    private final String port;
+
+    public RedissonConfig(
+        @Value("${spring.redis.host}") final String host,
+        @Value("${spring.redis.port}") final String port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public RedissonClient getRedissonConfig() {
+        final Config config = new Config();
+        config
+            .useSingleServer()
+            .setAddress("redis://" + host + ":" + port);
+
+        return Redisson.create(config);
+    }
+}
