c138db153d9b94bc8c3175129fe2ac4152de5666
jjungk <jsk33a@gmail.com>
2023-08-30 09:23:32 +0900

    refactor: 코드 컨벤션 적용

--- src/main/java/com/woowacamp/soolsool/core/statistics/repository/StatisticsRepository.java
+++ src/main/java/com/woowacamp/soolsool/core/statistics/repository/StatisticsRepository.java
@@ -14,21 +14,22 @@ public interface StatisticsRepository extends JpaRepository<Statistics, Statisti
     @Modifying
     @Query(value =
         "INSERT INTO statistics (liquor_id, year, month, week, day, sale_quantity, sale_price, brew_type, region) "
-            + "SELECT ri.liquor_id as liquor_id, "
-            + "YEAR(DATE(DATE_FORMAT(o.created_at, '%Y-%m-%d'))) as year, "
-            + "MONTH(DATE(DATE_FORMAT(o.created_at, '%Y-%m-%d'))) as month, "
-            + "WEEK(DATE(DATE_FORMAT(o.created_at, '%Y-%m-%d'))) as week, "
-            + "DAY(DATE(DATE_FORMAT(o.created_at, '%Y-%m-%d'))) as day, "
-            + "SUM(ri.quantity) as sale_quantity, "
-            + "SUM(ri.quantity * ri.liquor_original_price) as sale_price, "
-            + "ri.liquor_brew as brew_type, "
-            + "ri.liquor_region as region "
-            + "FROM receipt_items ri "
-            + "INNER JOIN orders o ON ri.receipt_id = o.receipt_id "
-            + "WHERE o.order_status_id = 1 "
-            + "AND o.created_at BETWEEN :startDate AND :endDate "
-            + "GROUP BY year, month, week, day, liquor_id, brew_type, region "
-            + "ON DUPLICATE KEY UPDATE sale_quantity = (SELECT sale_quantity), sale_price = (SELECT sale_price) ", nativeQuery = true)
+            +
+            "    SELECT ri.liquor_id as liquor_id, " +
+            "YEAR(o.created_at) as year, " +
+            "MONTH(o.created_at) as month, " +
+            "WEEK(o.created_at) as week, " +
+            "DAY(o.created_at) as day, " +
+            "SUM(ri.quantity) as sale_quantity, " +
+            "SUM(ri.quantity * ri.liquor_original_price) as sale_price, " +
+            "ri.liquor_brew as brew_type, " +
+            "ri.liquor_region as region " +
+            "FROM receipt_items ri " +
+            "INNER JOIN orders o ON ri.receipt_id = o.receipt_id " +
+            "WHERE o.order_status_id = 1 " +
+            "AND o.created_at BETWEEN :startDate AND :endDate " +
+            "GROUP BY year, month, week, day, liquor_id, brew_type, region " +
+            "ON DUPLICATE KEY UPDATE sale_quantity = (SELECT sale_quantity), sale_price = (SELECT sale_price) ", nativeQuery = true)
     void updateStatisticsSales(
         final LocalDate startDate,
         final LocalDate endDate
---
+++ src/test/java/com/woowacamp/soolsool/core/statistics/domain/StatisticsTest.java
@@ -0,0 +1,32 @@
+package com.woowacamp.soolsool.core.statistics.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import com.woowacamp.soolsool.core.statistics.domain.vo.BrewType;
+import com.woowacamp.soolsool.core.statistics.domain.vo.Click;
+import com.woowacamp.soolsool.core.statistics.domain.vo.Impression;
+import com.woowacamp.soolsool.core.statistics.domain.vo.Region;
+import com.woowacamp.soolsool.core.statistics.domain.vo.SalePrice;
+import com.woowacamp.soolsool.core.statistics.domain.vo.SaleQuantity;
+import java.math.BigInteger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("통계 단위 테스트")
+class StatisticsTest {
+
+    @Test
+    @DisplayName("객체 생성 성공")
+    void create() {
+        // when & then
+        assertThatNoException()
+            .isThrownBy(() -> Statistics.builder()
+                .salePrice(new SalePrice(new BigInteger("10000")))
+                .saleQuantity(new SaleQuantity(new BigInteger("100")))
+                .impression(new Impression(new BigInteger("1000")))
+                .region(new Region("서울"))
+                .brewType(new BrewType("과일주"))
+                .click(new Click(new BigInteger("120")))
+                .build());
+    }
+}
