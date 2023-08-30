dab8844d729a6c4f358dbe0f2756b66874d69117
worldii <bbc2788@naver.com>
2023-08-30 17:08:28 +0900

    refactor: disable 빼기

--- src/test/java/com/woowacamp/soolsool/core/member/service/MemberServiceTest.java
+++ src/test/java/com/woowacamp/soolsool/core/member/service/MemberServiceTest.java
@@ -21,7 +21,6 @@ import com.woowacamp.soolsool.core.member.repository.MemberRepository;
 import com.woowacamp.soolsool.core.member.repository.MemberRoleCache;
 import java.math.BigInteger;
 import java.util.Optional;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,7 +31,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @DisplayName("단위 테스트: Member")
 @ExtendWith(MockitoExtension.class)
-@Disabled
 class MemberServiceTest {
 
     @Mock
