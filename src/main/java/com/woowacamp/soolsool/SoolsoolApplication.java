3cd0543b1c4925b2ed057c99304010db6b311def
hwihwi523 <57697202+hwihwi523@users.noreply.github.com>
2023-08-31 10:43:21 +0900

    hotfix: Async 삭제, 락 획득 전 정렬 (#194)

--- src/main/java/com/woowacamp/soolsool/SoolsoolApplication.java
+++ src/main/java/com/woowacamp/soolsool/SoolsoolApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @EnableCaching
 @EnableScheduling
 @EnableJpaAuditing
