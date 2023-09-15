cbf9f9b8eeaf0ecae6d43d426036ebbd8ba01fba
Jongha Park <jongha2788@u.sogang.ac.kr>
2023-09-15 20:33:54 +0900

    refactor: 첫페이지 리팩터링

--- src/main/java/com/woowacamp/soolsool/core/liquor/repository/LiquorQueryDslRepository.java
+++ src/main/java/com/woowacamp/soolsool/core/liquor/repository/LiquorQueryDslRepository.java
@@ -2,14 +2,13 @@ package com.woowacamp.soolsool.core.liquor.repository;
 
 import static com.woowacamp.soolsool.core.liquor.domain.QLiquor.liquor;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.woowacamp.soolsool.core.liquor.domain.Liquor;
 import com.woowacamp.soolsool.core.liquor.domain.LiquorBrew;
 import com.woowacamp.soolsool.core.liquor.domain.LiquorRegion;
 import com.woowacamp.soolsool.core.liquor.domain.LiquorStatus;
 import com.woowacamp.soolsool.core.liquor.domain.vo.LiquorBrand;
-import com.woowacamp.soolsool.core.liquor.dto.LiquorElementResponse;
 import com.woowacamp.soolsool.core.liquor.dto.LiquorSearchCondition;
 import java.util.List;
 import java.util.Objects;
@@ -26,17 +25,12 @@ public class LiquorQueryDslRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<LiquorElementResponse> getList(
+    public List<Liquor> getList(
         final LiquorSearchCondition condition,
         final Pageable pageable,
         final Long cursorId
     ) {
-        return queryFactory.select(
-                Projections.constructor(
-                    LiquorElementResponse.class,
-                    liquor
-                )
-            )
+        return queryFactory.select(liquor)
             .from(liquor)
             .where(
                 eqRegion(condition.getLiquorRegion()),
@@ -51,22 +45,9 @@ public class LiquorQueryDslRepository {
     }
 
     @Cacheable(value = "liquorsFirstPage")
-    public List<LiquorElementResponse> getCachedList(
-        final Pageable pageable
-    ) {
+    public List<Liquor> getCachedList(final Pageable pageable) {
         log.info("LiquorQueryDslRepository getCachedList");
-        return getList(pageable, null);
-    }
-
-    private List<LiquorElementResponse> getFirstPageList(
-        final Pageable pageable
-    ) {
-        return queryFactory.select(
-                Projections.constructor(
-                    LiquorElementResponse.class,
-                    liquor
-                )
-            )
+        return queryFactory.select(liquor)
             .from(liquor)
             .orderBy(liquor.id.desc())
             .limit(pageable.getPageSize())
