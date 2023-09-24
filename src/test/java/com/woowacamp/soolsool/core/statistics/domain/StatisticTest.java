fbe063a9ac57df785cd779cbef97cbe55b3e4394
jsk33a <jsk33a@gmail.com>
2023-09-24 23:51:49 +0900

    fix: StatisticTest 수정

--- src/test/java/com/woowacamp/soolsool/core/statistics/domain/StatisticTest.java
+++ src/test/java/com/woowacamp/soolsool/core/statistics/domain/StatisticTest.java
@@ -1,16 +1,11 @@
 package com.woowacamp.soolsool.core.statistics.domain;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-
-import com.woowacamp.soolsool.core.statistics.domain.vo.BrewType;
-import com.woowacamp.soolsool.core.statistics.domain.vo.Click;
-import com.woowacamp.soolsool.core.statistics.domain.vo.Impression;
-import com.woowacamp.soolsool.core.statistics.domain.vo.Region;
-import com.woowacamp.soolsool.core.statistics.domain.vo.SalePrice;
-import com.woowacamp.soolsool.core.statistics.domain.vo.SaleQuantity;
-import java.math.BigInteger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @DisplayName("단위 테스트: Statistic")
 class StatisticTest {
@@ -20,13 +15,13 @@ class StatisticTest {
     void create() {
         // when & then
         assertThatNoException()
-            .isThrownBy(() -> Statistic.builder()
-                .salePrice(new SalePrice(new BigInteger("10000")))
-                .saleQuantity(new SaleQuantity(new BigInteger("100")))
-                .impression(new Impression(new BigInteger("1000")))
-                .region(new Region("서울"))
-                .brewType(new BrewType("과일주"))
-                .click(new Click(new BigInteger("120")))
-                .build());
+                .isThrownBy(() -> Statistic.builder()
+                        .salePrice(new BigInteger("10000"))
+                        .saleQuantity(new BigInteger("100"))
+                        .impression(new BigInteger("1000"))
+                        .region("서울")
+                        .brewType("과일주")
+                        .click(new BigInteger("120"))
+                        .build());
     }
 }
